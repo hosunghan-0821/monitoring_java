cdb86ba1efff734e0a75edbd19a686f2a5815dfd
hanhosung <winsomed96@naver.com>
2024-04-30 13:23:24 +0900

    fe: add Julian Integration Test

--- src/test/java/com/example/monitor/integration/JulianMonitorCoreIntegrationTest.java
+++ src/test/java/com/example/monitor/integration/JulianMonitorCoreIntegrationTest.java
@@ -5,24 +5,28 @@ import com.example.monitor.chrome.ChromeDriverToolFactory;
 import com.example.monitor.monitoring.julian.JulianFindString;
 import com.example.monitor.monitoring.julian.JulianMonitorCore;
 import com.example.monitor.monitoring.julian.JulianProduct;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Import({TestConfiguration.class})
 class JulianMonitorCoreIntegrationTest {
 
     @Value("${julian.user.id}")
@@ -38,51 +42,79 @@ class JulianMonitorCoreIntegrationTest {
     private ChromeDriverToolFactory chromeDriverToolFactory;
 
 
-    private ChromeDriver driver;
+    private static HashMap<String, JulianProduct> productHashMap;
+    private static ChromeDriver driver;
 
-    private WebDriverWait wait;
+    private static WebDriverWait wait;
 
-    @BeforeEach
-    void init() {
-        chromeDriverToolFactory.makeChromeDriverTool("test");
-        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool("test");
-        driver = chromeDriverTool.getChromeDriver();
-        wait = chromeDriverTool.getWebDriverWait();
+    @BeforeAll
+    static void init() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("window-size=1920x1080");
+        options.addArguments("start-maximized");
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("--disable-automation");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("detach", true);
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofMillis(5000)); // 최대 5초 대기
+        productHashMap = new HashMap<>();
+
     }
 
-    @AfterEach
-    void end() {
+    @AfterAll
+    static void end() {
         driver.quit();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName(JulianFindString.JULIAN_LOG_PREFIX + "데이터 로드 테스트")
+    void dataLoad() {
+
+        julianMonitorCore.login(driver, wait);
+        //when
+        List<JulianProduct> julianProductList = getJulianProducts(JulianFindString.ALL_CATEGORIES_URL);
+
+        julianMonitorCore.loadData(productHashMap, julianProductList);
+
+        //then
+        assertThat(productHashMap.size()).isGreaterThan(1);
+
     }
 
 
     @Test
+    @Order(2)
     @DisplayName(JulianFindString.JULIAN_LOG_PREFIX + "데이터 조회 및 원산지 확인 테스트")
     void getPageProductDataTest() {
         //given
-        julianMonitorCore.login(driver, wait);
+        List<JulianProduct> julianProductList = getJulianProducts(JulianFindString.ALL_CATEGORIES_URL);
 
-        String url = julianMonitorCore.getUrl(JulianFindString.ALL_CATEGORIES_URL, 1);
+        //then
+        for (JulianProduct julianProduct : julianProductList) {
+
+            assertThat(julianProduct.getSku()).isNotNull();
+            assertThat(julianProduct.getImageUrl()).isNotNull();
+            assertThat(julianProduct.getProductLink()).isNotNull();
+            assertThat(julianProduct.getName()).isNotNull();
+            assertThat(julianProduct.getPrice()).isNotNull();
+            assertThat(productHashMap.containsKey(julianProduct.getSku())).isEqualTo(true);
+        }
+    }
+
+
+
+
+    private List<JulianProduct> getJulianProducts(String category) {
+        String url = julianMonitorCore.getUrl(category, 1);
         julianMonitorCore.changeUrl(driver, url);
         List<WebElement> innerProductDivs = julianMonitorCore.getInnerProductDivs(wait);
         //when
         List<JulianProduct> julianProductList = julianMonitorCore.getProductData(innerProductDivs, url);
-
-        //then
-        JulianProduct julianProduct = julianProductList.get(0);
-
-        assertThat(julianProductList.size()).isGreaterThanOrEqualTo(1);
-
-        assertThat(julianProduct.getSku()).isNotNull();
-        assertThat(julianProduct.getImageUrl()).isNotNull();
-        assertThat(julianProduct.getProductLink()).isNotNull();
-        assertThat(julianProduct.getName()).isNotNull();
-        assertThat(julianProduct.getPrice()).isNotNull();
-
-        for (JulianProduct julianProductData : julianProductList) {
-            System.out.println(julianProductData.toString());
-        }
-
-
+        return julianProductList;
     }
 }
