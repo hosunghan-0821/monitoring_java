491da5bea0a16c534bd3c1f154201b543dc5c4d4
hanhosung <winsomed96@naver.com>
2024-05-01 14:54:13 +0900

    fe: add ChromeDriver test

---
+++ src/test/java/com/example/monitor/chrome/ChromeDriverToolFactoryTest.java
@@ -0,0 +1,32 @@
+package com.example.monitor.chrome;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ChromeDriverToolFactoryTest {
+
+    @InjectMocks
+    private ChromeDriverToolFactory chromeDriverToolFactory;
+
+    @Test
+    @DisplayName("[Chrome] : chromeDriverTool Test")
+    void makeChromeDriverTool() {
+        //given
+        chromeDriverToolFactory.makeChromeDriverTool("test");
+
+        //when
+        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool("test");
+        //then
+        Assertions.assertThat(chromeDriverTool.getChromeDriver()).isNotNull();
+        Assertions.assertThat(chromeDriverTool.getWebDriverWait()).isNotNull();
+
+        chromeDriverTool.getChromeDriver().quit();
+    }
+}
