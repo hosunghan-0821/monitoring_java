40bc8ae0d1938d86572e3885755b867a12b12d71
hanhosung <winsomed96@naver.com>
2025-04-19 10:08:09 +0900

    fix: 주석제거

--- monitor-core/src/main/java/com/example/monitor/scheduler/MonitorScheduler.java
+++ monitor-core/src/main/java/com/example/monitor/scheduler/MonitorScheduler.java
@@ -57,84 +57,84 @@ public class MonitorScheduler {
 
     private final EicMonitorCore eicMonitorCore;
 
-//
-//    @Scheduled(initialDelay = 60000 * 2, fixedDelay = 60000)// 1분마다 실행
-//    public void monitorJulianAllCategories() {
-//        MDC.put("threadName", JULIAN); // MDC에 쓰레드 이름 저장
-//        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(ALL_CATEGORIES);
-//        julianMonitorCore.runFindProductLogic(chromeDriverTool);
-//        MDC.clear(); // MDC 데이터 정리
-//    }
-//
-//    @Scheduled(initialDelay = 60000 * 7, fixedDelay = 60000 * 30)// 30분마다 실행
-//    public void monitorDoubleF() {
-//        MDC.put("threadName", DOUBLE_F); // MDC에 쓰레드 이름 저장
-//        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(DOUBLE_F);
-//        doubleFMonitorCore.runFindProductLogic(chromeDriverTool);
-//        MDC.clear(); // MDC 데이터 정리
-//    }
-//
-//    @Scheduled(initialDelay = 60000 * 5, fixedDelay = 60000 * 20)// 20분마다 실행
-//    public void monitorBIFFI() {
-//        MDC.put("threadName", BIFFI); // MDC에 쓰레드 이름 저장
-//        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(BIFFI);
-//        biffiMonitorCore.runFindProductLogic(chromeDriverTool);
-//        MDC.clear(); // MDC 데이터 정리
-//    }
-//
-//
-//    @Scheduled(initialDelay = 60000, fixedDelay = 60000 * 60 * 24 * 3)// 3일마다 실행
-//    public void clearKeySet() {
-//        //Julian
-//        log.info(JULIAN_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : All Categories, Promo");
-//        julianMonitorCore.getJulianBrandHashData().getProductKeySet().clear();
-//
-//        //DoubleF
-//        log.info(DOUBLE_F_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : DoubleF ");
-//        doubleFMonitorCore.getDoubleFBrandHashData().getProductKeySet().clear();
-//
-//        //biffi
-//        log.info(BIFFI_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : biffi ");
-//        biffiMonitorCore.getBiffiBrandHashData().getProductKeySet().clear();
-//
-//        //Vietti
-//        log.info(VIETTI_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : Vietti");
-//        viettiMonitorCore.getViettiBrandHashData().getProductKeySet().clear();
-//
-//        //Style
-//        log.info(STYLE_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : Style");
-//        styleMonitorCore.getStyleBrandHashData().getProductKeySet().clear();
-//
-//        //eic
-//        log.info(EicFindString.EIC_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : eic");
-//        eicMonitorCore.getEicBrandHashData().getProductKeySet().clear();
-//
-//    }
-//
-//    @Scheduled(initialDelay = 60000 * 4, fixedDelay = 60000 * 5)// 30분마다 실행
-//    public void monitorGebene() {
-//        MDC.put("threadName", GEBE); // MDC에 쓰레드 이름 저장
-//        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(GEBE);
-//        gebenegoziMonitorCore.runFindProductLogic(chromeDriverTool);
-//        MDC.clear(); // MDC 데이터 정리
-//    }
-//
-//    @Scheduled(initialDelay = 60000 * 4, fixedDelay = 60000 * 5)// 5분마다 실행
-//    public void monitorStyle() {
-//        MDC.put("threadName", STYLE); // MDC에 쓰레드 이름 저장
-//        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(STYLE);
-//        styleMonitorCore.runFindProductLogic(chromeDriverTool);
-//        MDC.clear(); // MDC 데이터 정리
-//    }
-//
-//    @Scheduled(initialDelay = 60000 * 4, fixedDelay = 60000 * 5)// 5분마다 실행
-//    public void monitorVietti() {
-//        MDC.put("threadName", VIETTI); // MDC에 쓰레드 이름 저장
-//        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(VIETTI);
-//        viettiMonitorCore.runFindProductLogic(chromeDriverTool);
-//        MDC.clear(); // MDC 데이터 정리
-//
-//    }
+
+    @Scheduled(initialDelay = 60000 * 2, fixedDelay = 60000)// 1분마다 실행
+    public void monitorJulianAllCategories() {
+        MDC.put("threadName", JULIAN); // MDC에 쓰레드 이름 저장
+        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(ALL_CATEGORIES);
+        julianMonitorCore.runFindProductLogic(chromeDriverTool);
+        MDC.clear(); // MDC 데이터 정리
+    }
+
+    @Scheduled(initialDelay = 60000 * 7, fixedDelay = 60000 * 30)// 30분마다 실행
+    public void monitorDoubleF() {
+        MDC.put("threadName", DOUBLE_F); // MDC에 쓰레드 이름 저장
+        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(DOUBLE_F);
+        doubleFMonitorCore.runFindProductLogic(chromeDriverTool);
+        MDC.clear(); // MDC 데이터 정리
+    }
+
+    @Scheduled(initialDelay = 60000 * 5, fixedDelay = 60000 * 20)// 20분마다 실행
+    public void monitorBIFFI() {
+        MDC.put("threadName", BIFFI); // MDC에 쓰레드 이름 저장
+        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(BIFFI);
+        biffiMonitorCore.runFindProductLogic(chromeDriverTool);
+        MDC.clear(); // MDC 데이터 정리
+    }
+
+
+    @Scheduled(initialDelay = 60000, fixedDelay = 60000 * 60 * 24 * 3)// 3일마다 실행
+    public void clearKeySet() {
+        //Julian
+        log.info(JULIAN_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : All Categories, Promo");
+        julianMonitorCore.getJulianBrandHashData().getProductKeySet().clear();
+
+        //DoubleF
+        log.info(DOUBLE_F_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : DoubleF ");
+        doubleFMonitorCore.getDoubleFBrandHashData().getProductKeySet().clear();
+
+        //biffi
+        log.info(BIFFI_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : biffi ");
+        biffiMonitorCore.getBiffiBrandHashData().getProductKeySet().clear();
+
+        //Vietti
+        log.info(VIETTI_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : Vietti");
+        viettiMonitorCore.getViettiBrandHashData().getProductKeySet().clear();
+
+        //Style
+        log.info(STYLE_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : Style");
+        styleMonitorCore.getStyleBrandHashData().getProductKeySet().clear();
+
+        //eic
+        log.info(EicFindString.EIC_LOG_PREFIX + "새 등록 상품 풀 초기화 (중복방지용) : eic");
+        eicMonitorCore.getEicBrandHashData().getProductKeySet().clear();
+
+    }
+
+    @Scheduled(initialDelay = 60000 * 4, fixedDelay = 60000 * 5)// 30분마다 실행
+    public void monitorGebene() {
+        MDC.put("threadName", GEBE); // MDC에 쓰레드 이름 저장
+        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(GEBE);
+        gebenegoziMonitorCore.runFindProductLogic(chromeDriverTool);
+        MDC.clear(); // MDC 데이터 정리
+    }
+
+    @Scheduled(initialDelay = 60000 * 4, fixedDelay = 60000 * 5)// 5분마다 실행
+    public void monitorStyle() {
+        MDC.put("threadName", STYLE); // MDC에 쓰레드 이름 저장
+        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(STYLE);
+        styleMonitorCore.runFindProductLogic(chromeDriverTool);
+        MDC.clear(); // MDC 데이터 정리
+    }
+
+    @Scheduled(initialDelay = 60000 * 4, fixedDelay = 60000 * 5)// 5분마다 실행
+    public void monitorVietti() {
+        MDC.put("threadName", VIETTI); // MDC에 쓰레드 이름 저장
+        ChromeDriverTool chromeDriverTool = chromeDriverToolFactory.getChromeDriverTool(VIETTI);
+        viettiMonitorCore.runFindProductLogic(chromeDriverTool);
+        MDC.clear(); // MDC 데이터 정리
+
+    }
 
     @Scheduled(initialDelay = 60000 * 10, fixedDelay = 60000 * 30)// 5분마다 실행
     public void monitorEic() {
